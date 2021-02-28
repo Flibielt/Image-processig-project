4fb04878d16ec8c7804d7626cf0014ec0a963871
Csaba Hermann <hcsabi98@gmail.com>
2021-02-28 20:08:06 +0100

    Refactor: moved data folder

--- ocr/src/Ocr.py
+++ ocr/src/Ocr.py
@@ -7,7 +7,7 @@ class Ocr:
         self.image = None
 
     def read_image(self):
-        self.image = cv2.imread("ocr/src/data/text.png", cv2.IMREAD_COLOR)
+        self.image = cv2.imread("data/text.png", cv2.IMREAD_COLOR)
 
     def show_image(self):
         cv2.imshow("Text", self.image)
