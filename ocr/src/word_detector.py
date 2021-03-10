8af2e85a4d297b71434b4239ec7932bc0785281b
Csaba Hermann <hcsabi98@gmail.com>
2021-03-10 23:37:38 +0100

    Bug fixes in the segmentation
    Histogram calculation and histogram analysis

--- ocr/src/word_detector.py
+++ ocr/src/word_detector.py
@@ -1,8 +1,51 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import cv2
 
-LOW_HIST_VALUE = 50
-HIGH_HIST_VALUE = 150
+
+def get_word_borders(histogram, threshold):
+    border = []
+
+    word = False
+    for index in range(1, len(histogram) - 1):
+        if histogram[index] > threshold:
+            if not word:
+                border.append(index)
+                word = True
+
+        elif word:
+            border.append(index)
+            word = False
+
+    return border
+
+
+def calculate_horizontal_histogram(image):
+    im = 255 - image
+    height, width = image.shape[:2]
+
+    histogram = np.zeros(height)
+
+    for y in range(1, height):
+        for x in range(1, width):
+            if im[y, x] > 10:
+                histogram[y] = histogram[y] + 1
+
+    return histogram
+
+
+def calculate_vertical_histogram(image):
+    im = 255 - image
+    height, width = image.shape[:2]
+
+    histogram = np.zeros(width)
+
+    for x in range(1, width):
+        for y in range(1, height):
+            if im[y, x] > 3:
+                histogram[x] = histogram[x] + 2
+
+    return histogram
 
 
 class WordDetector:
@@ -23,20 +66,12 @@ class WordDetector:
         :return: The boundaries of the rows
         """
 
-        horizontal_hist = self.image.shape[1] - np.sum(self.image, axis=1) / 255
+        horizontal_hist = calculate_horizontal_histogram(self.image)
 
         plt.plot(horizontal_hist)
         plt.show()
 
-        lines = []
-        line_count = 0
-        for index in range(1, len(horizontal_hist)):
-            if horizontal_hist[index] < LOW_HIST_VALUE:
-                if horizontal_hist[index - 1] > HIGH_HIST_VALUE or horizontal_hist[index + 1] > HIGH_HIST_VALUE:
-                    lines[line_count] = index
-                    line_count = line_count + 1
-
-        return lines
+        return get_word_borders(horizontal_hist, 40)
 
     def get_cols(self, rows):
         """
@@ -47,23 +82,28 @@ class WordDetector:
         """
 
         words = []
-        word_count = 0
         for row in range(0, len(rows), 2):
             width = self.image.shape[1]
             image_row = self.image[rows[row]:rows[row + 1], 0:width]
-            row_height = image_row.shape[0]
-            vertical_hist = image_row.shape[0] - np.sum(image_row, axis=0) / 255
 
-            columns = []
-            column_count = 0
-            for index in range(1, len(vertical_hist)):
-                if vertical_hist[index] < LOW_HIST_VALUE:
-                    if vertical_hist[index] > HIGH_HIST_VALUE or vertical_hist[index] > HIGH_HIST_VALUE:
-                        columns[column_count] = index
-                        column_count = column_count + 1
+            cv2.imshow("row", image_row)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+            row_height = image_row.shape[0]
+
+            vertical_hist = calculate_vertical_histogram(image_row)
+
+            columns = get_word_borders(vertical_hist, 3)
+
+            plt.plot(vertical_hist)
+            plt.show()
 
             for index in range(0, len(columns), 2):
-                words[word_count] = image_row[row_height:row_height, columns[index]:columns[index + 1]]
-                word_count = word_count + 1
+                words.append(image_row[0:row_height, columns[index]:columns[index + 1]])
+
+        cv2.imshow("first", words[0])
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
         return words
