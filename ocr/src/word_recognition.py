97f5da2c920be8280b221dbe983a36f05c1cde68
Csaba Hermann <hcsabi98@gmail.com>
2021-04-25 00:16:44 +0200

    Created WordRecognizing class

---
+++ ocr/src/word_recognition.py
@@ -0,0 +1,37 @@
+import sys
+
+from .etalon import Etalon
+
+
+def calculate_distance(word, etalon):
+    distance = 0
+    # todo: Calculate distance
+
+    return distance
+
+
+class WordRecognizing:
+    def __init__(self):
+        self.etalon = Etalon()
+
+    def recognize_words(self, words):
+        etalon_matrix = self.etalon.etalon_matrix
+        text = ""
+
+        for word in words:
+            min_distance = sys.maxsize
+            min_position = 0
+
+            for i in range(0, etalon_matrix):
+                distance = calculate_distance(word.feature_vector, etalon_matrix[i].feature_vector)
+
+                if distance < min_distance:
+                    min_distance = distance
+                    min_position = i
+
+            word.text = etalon_matrix[min_position].text
+
+        for word in words:
+            text = text + word.text
+
+        return text
