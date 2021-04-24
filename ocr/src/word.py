43de2f1fef35329688ecd4eb16505e503204faa4
Csaba Hermann <hcsabi98@gmail.com>
2021-04-24 23:05:36 +0200

    Added feature vector field to the Word class

--- ocr/src/word.py
+++ ocr/src/word.py
@@ -2,6 +2,7 @@ class Word:
     def __init__(self):
         self.image = None
         self.resized_image = None
+        self.feature_vector = None
         self.text = ""
         self.x = 0
         self.y = 0
