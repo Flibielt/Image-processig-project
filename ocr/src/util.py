ec97c85b1eed47fb36d849aadd8bfa48c5eecdbe
Csaba Hermann <hcsabi98@gmail.com>
2021-04-29 13:18:10 +0200

    Created save image function

--- ocr/src/util.py
+++ ocr/src/util.py
@@ -1,9 +1,14 @@
 import cv2
-
-THRESHOLD = 3
+import os
 
 
 def show_image(text, image):
     cv2.imshow(text, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def save_image(name, image):
+    home_dir = os.path.expanduser("~")
+    file_path = os.path.join(home_dir, "Documents", name + ".png")
+    cv2.imwrite(file_path, image)
