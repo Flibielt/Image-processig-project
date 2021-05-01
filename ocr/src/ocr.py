5c4408c30a03412f88d5c4638bf361bf856ea429
Csaba Hermann <hcsabi98@gmail.com>
2021-05-01 18:29:55 +0200

    Moved the save image function to the util file

--- ocr/src/ocr.py
+++ ocr/src/ocr.py
@@ -4,6 +4,7 @@ import cv2
 
 from .grayscale_converter import GrayscaleConverter
 from .image_filter import ImageFilter
+from .util import show_image, save_image
 from .word_detector import WordDetector
 from .word_recognition import WordRecognizing
 
@@ -17,24 +18,19 @@ class Ocr:
     def __init__(self):
         self.image = None
 
-    def save_image(self):
-        home_dir = os.path.expanduser("~")
-        file_path = os.path.join(home_dir, "Documents", "savedText.png")
-        cv2.imwrite(file_path, self.image)
-
     def preprocess_image(self):
         grayscale_image = grayscale_converter.process_image(self.image)
-        # show_image("Grayscale image", grayscale_image)
+        show_image("Grayscale image", grayscale_image)
 
         filtered_image = image_filter.filter_image(grayscale_image)
-        # show_image("Filtered image", filtered_image)
+        show_image("Filtered image", filtered_image)
 
         return filtered_image
 
     def process_image(self, image_name):
         print("Processing image...")
         self.image = cv2.imread(image_name, cv2.IMREAD_COLOR)
-        self.save_image()
+        save_image("original", self.image)
 
         filtered_image = self.preprocess_image()
 
