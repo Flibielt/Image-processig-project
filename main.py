05ee38c42a99f1d36e9d1b2f5bf758c4c4654a98
Csaba Hermann <hcsabi98@gmail.com>
2021-05-06 14:23:19 +0200

    Call the process for sample image

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from ocr.src import Ocr
 
 def main():
     ocr = Ocr()
-    text = ocr.process_image("data/printed.jpg")
-    # text = ocr.process_image("data/sample.png")
+    text = ocr.process_image("data/fox_easy.png")
+    # text = ocr.process_image("data/printed.jpg")
+    # text = ocr.process_image("data/printed_3.png")
+    # text = ocr.process_image("data/printed_4.png")
     print(text)
 
 
